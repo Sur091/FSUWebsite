6390e860baf932d0492924aa2b15c2cae7cde56a
Sur091 <acharyasuraj091@gmail.com>
2023-06-23 19:08:38 +0545

    0.4.2 animation partially complete

--- js/script.js
+++ js/script.js
@@ -1,3 +1,24 @@
+
+
+const content = document.querySelectorAll("body");
+const buttons =  document.querySelectorAll(".control")
+// console.log(controls);
+
 function toggleMenu() {
     
 }
+
+function page_transition() {
+    // console.log(buttons);
+    for (let i = 0; i < buttons.length; i++) {
+        // console.log(buttons[i]);
+        buttons[i].addEventListener('click', (e) => {
+            for (let j = 0; j < buttons.length; j++) {
+                buttons[j].classList.remove('active-btn');
+            }
+            buttons[i].classList.add('active-btn');
+        })
+    }
+}
+
+page_transition();
