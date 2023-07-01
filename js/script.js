445f04637905b168501b6fde2bd1b91815f409aa
Sur091 <acharyasuraj091@gmail.com>
2023-07-01 07:22:01 +0545

    0.4.3 about and home

--- js/script.js
+++ js/script.js
@@ -2,23 +2,47 @@
 
 const content = document.querySelectorAll("body");
 const buttons =  document.querySelectorAll(".control")
-// console.log(controls);
+const text = document.querySelectorAll(".text-control")
+const toggle_nav = document.getElementById("toggle-nav")
+console.log(toggle_nav.classList);
 
-function toggleMenu() {
-    
+function showToggleMenu() {
+    console.log
+    document.addEventListener('mousemove', (e) => {
+        let xPos = e.clientX;
+        if (xPos < 70 && toggle_nav.classList.contains('hide')){
+            toggle_nav.classList.remove('hide');
+        }
+    })
 }
 
+function closeToggleMenu() {
+    document.addEventListener('mousemove', (e) => {
+        let xPos = e.clientX;
+        if (xPos > 240 && !toggle_nav.classList.contains('hide')){
+            toggle_nav.classList.add('hide');
+        }
+    })
+}
+
+
 function page_transition() {
-    // console.log(buttons);
+    // console.log(text);
     for (let i = 0; i < buttons.length; i++) {
         // console.log(buttons[i]);
         buttons[i].addEventListener('click', (e) => {
+            // console.log(buttons[i]);
             for (let j = 0; j < buttons.length; j++) {
                 buttons[j].classList.remove('active-btn');
+                text[j].classList.remove('active-text')
             }
             buttons[i].classList.add('active-btn');
+            text[i].classList.add('active-text')
+            // console.log(buttons[i], text[i]);
         })
     }
 }
 
 page_transition();
+closeToggleMenu();
+showToggleMenu();
